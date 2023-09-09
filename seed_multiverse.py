97927d858504518384f8bea604ce5176e814de36
Lee <aspitarte@gmail.com>
2023-09-08 19:55:35 -0700

    clean and comment

--- seed_multiverse.py
+++ seed_multiverse.py
@@ -3,6 +3,8 @@ import os
 import re
 import numpy as np
 import pandas as pd
+
+from utils import transition_fn_from_transition_row, clip_names_from_transition_row
 # %%
 
 import argparse
@@ -20,44 +22,30 @@ num_output_rows = int(args.num_output_rows)
 # scene = 'tram_liftoff'
 # num_output_rows = 10
 
-from dotenv import load_dotenv, dotenv_values
-load_dotenv()  # take environment variables from .env.
+from dotenv import load_dotenv; load_dotenv()
 gdrive_basedir = os.getenv('base_dir')
-# gdrive_basedir = r'G:\.shortcut-targets-by-id\1Dpm6bJCMAI1nDoB2f80urmBCJqeVQN8W\AI-Art Kyle'
-
 input_basedir = os.path.join(gdrive_basedir, '{}\scenes'.format(song))
 
-#TODO: Rename this 'folder'
-folder = os.path.join(input_basedir, scene)
+scene_folder = os.path.join(input_basedir, scene)
 
-# folder = os.path.join(gdrive_basedir,r'elationstation\transitions' )
-
-if not os.path.exists(folder):
+if not os.path.exists(scene_folder):
     raise ValueError("did not find input scene folder")
 
-folder_rev = os.path.join(folder, 'rev')
+folder_rev = os.path.join(scene_folder, 'rev')
 
 #%%
+# Load list of all transitions
+#TODO: This needs to be pulled out of the excel file
 
 fn = 'input_data.xlsx'
 fp = os.path.join(gdrive_basedir, fn)
 df = pd.read_excel(fp, sheet_name='transitions_{}'.format(song), index_col=0)
 
-
 df = df.where(df['scene'] == scene).dropna(how='all')
-# df = df.dropna(subset=['start'])
-
 
 # %%
-import re
 
-from utils import transition_fn_from_transition_row, clip_names_from_transition_row
-
-
-# for fn in fns:
-#     get_clips(fp)
-
-
+# Determine clip names for each transition
 
 c1s = []
 c2s = []
@@ -70,41 +58,27 @@ for idx, row in df.iterrows():
     df.loc[idx, 'c2'] = c2
     df.loc[idx, 'fn'] = fn
 
-    # break
-
-# print(df)
 df
 #%%
 
 set(df['c1']) == set(df['c2']) 
 #%%
 
+# Make a lookup table for each clip
+
 all_cs = list(set([*df['c1'], *df['c2']]))
-
-all_cs
-
-#%%%
-
-
-
-
-seeds= list(set())
 
 c_id = list(range(len(all_cs)))
 
 seed_lookup = pd.Series(all_cs, index=c_id)
 seed_lookup.name ='seed_str'
-seed_lookup
 
-#%%
-
-# idx_lookup = seed_lookup.reset_index()
 idx_lookup = seed_lookup.reset_index().set_index('seed_str')['index']
 idx_lookup
 
-
 # %%
 
+# Generate the sequence of transitions in terms of clip index
 
 num_videos = len(seed_lookup)
 
@@ -141,8 +115,6 @@ for i in df_transitions.index:
             found_match = True
             break
 
-
-    print(found_match)
     df_transitions['idx_1'][i] = cur_idx
     df_transitions['idx_2'][i] = next_idx
 
@@ -153,24 +125,19 @@ df_transitions
     
 #%%
 
+# lookup the clips for each transition, and whether they should the reversed clip
+
 vals = df[['c1', 'c2']].values
 
 vals = [(f, t) for f, t in vals]
 
-# print(vals)
 reverse = []
 from_seed = []
 to_seed = []
 
 for transition_index, row in df_transitions.iterrows():
 
-
-    # pass
     idx_tup = (seed_lookup[row['idx_1']], seed_lookup[row['idx_2']])
-
-
-    # idx_tup = (seed_lookup[i] for i in idx_tup)
-
 
     if idx_tup in vals:
         reverse.append(False)
@@ -181,51 +148,30 @@ for transition_index, row in df_transitions.iterrows():
         from_seed.append(idx_tup[1])
         to_seed.append(idx_tup[0])
 
-
-
-
-#     # df_transitions.loc[transition_index][['idx_1', 'c2']] = idx_tup
 df_transitions['reverse'] = reverse
 df_transitions['from_seed'] = from_seed
 df_transitions['to_seed'] = to_seed
 
-
 df_transitions
 
-
-
-
-
 # %%
+
+# Find the file path associated with each transition, which depends on whether that is reversed
+
 df_transitions["fn"]=df_transitions['from_seed']+ ' to ' +df_transitions['to_seed']+'.mp4'
 
-
-# output_folder = [folder_rev if reverse else folder for reverse in df_transitions['reverse']]
 input_folder = ['transitions_rev' if reverse else 'transitions' for reverse in df_transitions['reverse']]
-
-
 input_basedir = os.path.join(gdrive_basedir, song)
-
-
-# output_folder = ['rev' if re]
 
 df_transitions['output_folder'] = input_folder
 
-
 df_transitions['fp_out'] = input_basedir + '\\' + df_transitions['output_folder'] + '\\' +  df_transitions['fn']
-
-
-
-df_transitions['fp_out'].values
-
-# df_transitions.to_csv('transitions.csv')
-
-
-#%%
 
 df_transitions['fp_out'].values
 
 # %%
+
+# Check if any transition files are missing
 
 df_exist = df_transitions['fp_out'].apply(os.path.exists)
 df_not_exist =df_transitions.where(df_exist == False).dropna(how='all')
@@ -235,23 +181,14 @@ if len(df_not_exist):
     print("Files not existing:  ")
     raise ValueError(df_not_exist[['output_folder', 'fn']])
 
-
-# %%
-
-
 df_transitions['fp_out']  = df_transitions['fp_out'].str.replace('\\','/', regex=False)
 #%%
-# df_transitions['fp_out'].groupby('index').apply("\nfile ".join)
-
-# out_txt = 'file ' + "\nfile ".join("\"" + df_transitions['fp_out'].str.replace(' ', '\ ') + '\"')
 out_txt = 'file ' + "\nfile ".join(df_transitions['fp_out'].str.replace(' ', '\ '))
-
-# out_txt = out_txt.replace(' ', '\ ')
 
 with open('videos.txt', 'w') as f:
     f.write(out_txt)
 
 import shutil
 
-shutil.move('videos.txt', os.path.join(folder, 'videos.txt'))
+shutil.move('videos.txt', os.path.join(scene_folder, 'videos.txt'))
 # %%
