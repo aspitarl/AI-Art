e2f0bbc46be2df0963e295cfafa2b447d5a90acf
aspitarl <aspitarte@gmail.com>
2023-09-16 22:02:43 -0700

    some cleaning of seed multiverse

--- seed_multiverse.py
+++ seed_multiverse.py
@@ -8,19 +8,22 @@ from utils import transition_fn_from_transition_row, clip_names_from_transition_
 # %%
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("song")
-parser.add_argument("scene")
-parser.add_argument("num_output_rows")
-args = parser.parse_args()
 
-song = args.song
-scene = args.scene
-num_output_rows = int(args.num_output_rows)
+USE_DEFAULT_ARGS = False
+if USE_DEFAULT_ARGS:
+    song = 'spacetrain_1024'
+    scene = 's1'
+    num_output_rows = int(10)
+else:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("song")
+    parser.add_argument("scene")
+    parser.add_argument("num_output_rows")
+    args = parser.parse_args()
 
-# song = 'spacetrain'
-# scene = 'tram_liftoff'
-# num_output_rows = 10
+    song = args.song
+    scene = args.scene
+    num_output_rows = int(args.num_output_rows)
 
 from dotenv import load_dotenv; load_dotenv()
 gdrive_basedir = os.getenv('base_dir')
@@ -35,38 +38,24 @@ folder_rev = os.path.join(scene_folder, 'rev')
 
 #%%
 # Load list of all transitions
-#TODO: This needs to be pulled out of the excel file
 
-fn = 'input_data.xlsx'
-fp = os.path.join(gdrive_basedir, fn)
-df = pd.read_excel(fp, sheet_name='transitions_{}'.format(song), index_col=0)
-
-df = df.where(df['scene'] == scene).dropna(how='all')
-
-# %%
+fp = os.path.join(gdrive_basedir, song, 'all_transitions.csv')
+df_transitions = pd.read_csv(fp, index_col=0).dropna(how='all')
 
 # Determine clip names for each transition
+df_transitions['fn'] = df_transitions.apply(transition_fn_from_transition_row, axis=1)
+df_transitions[['c1','c2']] = df_transitions.apply(clip_names_from_transition_row, axis=1, result_type='expand')
 
-c1s = []
-c2s = []
-for idx, row in df.iterrows():
+#This gets rid of all interscene transitions
+df_transitions = df_transitions.where(df_transitions['scene'] == scene).dropna(how='all')
 
-    fn = transition_fn_from_transition_row(row)
-    c1, c2 = clip_names_from_transition_row(row)
+df_transitions
 
-    df.loc[idx, 'c1'] = c1
-    df.loc[idx, 'c2'] = c2
-    df.loc[idx, 'fn'] = fn
-
-df
-#%%
-
-set(df['c1']) == set(df['c2']) 
 #%%
 
 # Make a lookup table for each clip
 
-all_cs = list(set([*df['c1'], *df['c2']]))
+all_cs = list(set([*df_transitions['c1'], *df_transitions['c2']]))
 
 c_id = list(range(len(all_cs)))
 
@@ -93,14 +82,14 @@ cur_idx = 0
 
 transition_idxs = []
 
-df_transitions = pd.DataFrame(columns = ['idx_1','idx_2'], index = list(range(num_output_rows)))
+df_trans_sequence = pd.DataFrame(columns = ['idx_1','idx_2'], index = list(range(num_output_rows)))
 
-trans_names_forward = df['c1'] + df['c2']
+trans_names_forward = df_transitions['c1'] + df_transitions['c2']
 trans_names_forward = trans_names_forward.values
-trans_names_rev = df['c2'] + df['c1']
+trans_names_rev = df_transitions['c2'] + df_transitions['c1']
 trans_names_rev = trans_names_rev.values
 
-for i in df_transitions.index:  
+for i in df_trans_sequence.index:  
 
     found_match = False
 
@@ -115,19 +104,19 @@ for i in df_transitions.index:
             found_match = True
             break
 
-    df_transitions['idx_1'][i] = cur_idx
-    df_transitions['idx_2'][i] = next_idx
+    df_trans_sequence['idx_1'][i] = cur_idx
+    df_trans_sequence['idx_2'][i] = next_idx
 
     cur_idx=next_idx
 
 
-df_transitions
+df_trans_sequence
     
 #%%
 
 # lookup the clips for each transition, and whether they should the reversed clip
 
-vals = df[['c1', 'c2']].values
+vals = df_transitions[['c1', 'c2']].values
 
 vals = [(f, t) for f, t in vals]
 
@@ -135,7 +124,7 @@ reverse = []
 from_seed = []
 to_seed = []
 
-for transition_index, row in df_transitions.iterrows():
+for transition_index, row in df_trans_sequence.iterrows():
 
     idx_tup = (seed_lookup[row['idx_1']], seed_lookup[row['idx_2']])
 
@@ -148,42 +137,42 @@ for transition_index, row in df_transitions.iterrows():
         from_seed.append(idx_tup[1])
         to_seed.append(idx_tup[0])
 
-df_transitions['reverse'] = reverse
-df_transitions['from_seed'] = from_seed
-df_transitions['to_seed'] = to_seed
+df_trans_sequence['reverse'] = reverse
+df_trans_sequence['from_seed'] = from_seed
+df_trans_sequence['to_seed'] = to_seed
 
-df_transitions
+df_trans_sequence
 
 # %%
 
 # Find the file path associated with each transition, which depends on whether that is reversed
 
-df_transitions["fn"]=df_transitions['from_seed']+ ' to ' +df_transitions['to_seed']+'.mp4'
+df_trans_sequence["fn"]=df_trans_sequence['from_seed']+ ' to ' +df_trans_sequence['to_seed']+'.mp4'
 
-input_folder = ['transitions_rev' if reverse else 'transitions' for reverse in df_transitions['reverse']]
+input_folder = ['transitions_rev' if reverse else 'transitions' for reverse in df_trans_sequence['reverse']]
 input_basedir = os.path.join(gdrive_basedir, song)
 
-df_transitions['output_folder'] = input_folder
+df_trans_sequence['output_folder'] = input_folder
 
-df_transitions['fp_out'] = input_basedir + '\\' + df_transitions['output_folder'] + '\\' +  df_transitions['fn']
+df_trans_sequence['fp_out'] = input_basedir + '\\' + df_trans_sequence['output_folder'] + '\\' +  df_trans_sequence['fn']
 
-df_transitions['fp_out'].values
+df_trans_sequence['fp_out'].values
 
 # %%
 
 # Check if any transition files are missing
 
-df_exist = df_transitions['fp_out'].apply(os.path.exists)
-df_not_exist =df_transitions.where(df_exist == False).dropna(how='all')
+df_exist = df_trans_sequence['fp_out'].apply(os.path.exists)
+df_not_exist =df_trans_sequence.where(df_exist == False).dropna(how='all')
 
 #TODO: move file exist checks to original for loop, such that it can keep trying to make a valid superscene with partial transitions. 
 if len(df_not_exist):
     print("Files not existing:  ")
     raise ValueError(df_not_exist[['output_folder', 'fn']])
 
-df_transitions['fp_out']  = df_transitions['fp_out'].str.replace('\\','/', regex=False)
+df_trans_sequence['fp_out']  = df_trans_sequence['fp_out'].str.replace('\\','/', regex=False)
 #%%
-out_txt = 'file ' + "\nfile ".join(df_transitions['fp_out'].str.replace(' ', '\ '))
+out_txt = 'file ' + "\nfile ".join(df_trans_sequence['fp_out'].str.replace(' ', '\ '))
 
 with open('videos.txt', 'w') as f:
     f.write(out_txt)
