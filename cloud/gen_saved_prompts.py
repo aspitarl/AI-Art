d16450979d735290a48e4fa535d23c19d3c75858
Lee Aspitarte <aspitarte@gmail.com>
2023-09-29 02:27:55 +0000

    add model cache

--- cloud/gen_saved_prompts.py
+++ cloud/gen_saved_prompts.py
@@ -25,7 +25,8 @@ from diffusers import StableDiffusionPipeline
 
 pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2-1",
                                                torch_dtype=torch.float16,
-                                               safety_checker=None
+                                               safety_checker=None,
+                                               cache_dir='model_cache'
                                                )
 
 
