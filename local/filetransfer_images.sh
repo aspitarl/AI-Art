5761849c141cc8f31e98e70d0127370963c9f9d7
aspitarl <aspitarte@gmail.com>
2023-09-28 19:34:27 -0700

    fix filetransfer folder output

--- local/filetransfer_images.sh
+++ local/filetransfer_images.sh
@@ -1,5 +1,8 @@
 : ${@?no positional parameters}
 source ../.env
 
-cd "$base_dir\\$song_name"
-scp -r -i $ssh_key_path $ssh_user@$ssh_ip_address:~/AI-Art/cloud/output/ .
+song_name=$1
+
+cd "$base_dir"
+
+scp -r -i $ssh_key_path $ssh_user@$ssh_ip_address:~/AI-Art/cloud/output/$song_name/ .
