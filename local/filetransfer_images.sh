f823dab511640bb6a6fc10ac95e329cbcaf530fc
Lee <aspitarte@gmail.com>
2023-10-27 13:42:21 -0700

    reorganize only using nx local

--- local/filetransfer_images.sh
+++
@@ -1,8 +0,0 @@
-: ${@?no positional parameters}
-source ../.env
-
-song_name=$1
-
-# cd "$base_dir"
-
-scp -r -i $ssh_key_path $ssh_user@$ssh_ip_address:~/AI-Art/cloud/output/$song_name/ "$base_dir"
--- local/gen_all_transitions.sh
+++
@@ -1,8 +0,0 @@
-: ${@?no positional parameters}
-# https://stackoverflow.com/questions/5228345/how-can-i-reference-a-file-for-variables-using-bash
-source ../.env
-
-song_name=$1
-
-python gen_intrascene_transitions.py $song_name
-python gen_interscene_transitions.py $song_name
