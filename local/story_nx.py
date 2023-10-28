433aaaf8458be5d2dbeffdd9210614fb1e6cd6ae
aspitarl <aspitarte@gmail.com>
2023-10-27 18:24:38 -0700

    refactor story_nx to new functions
    not working

--- local/story_nx.py
+++ local/story_nx.py
@@ -1,5 +1,3 @@
-
-
 #%%
 import os
 from os.path import join as pjoin
@@ -10,22 +8,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 from aa_utils.local import transition_fn_from_transition_row, clip_names_from_transition_row, image_names_from_transition
+from aa_utils.story import read_scene_dict, downselect_to_scene_sequence, gen_path_edges_short, construct_input_image_folder_paths, check_input_image_folders_exist, generate_text_for_ffmpeg, generate_output_video
 # %%
 
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("song", default='cycle_mask_test', nargs='?')
-parser.add_argument('--ss', default='scene_sequence', dest='scene_sequence')
+parser.add_argument('--ss', default='scene_sequence_kv3', dest='scene_sequence')
 parser.add_argument("-n", default=0, type=int, dest='N_repeats')
 parser.add_argument('-o', default='story_long.mov', dest='output_filename')
 args = parser.parse_args()
 # args = parser.parse_args("") # Needed for jupyter notebook
 
 N_repeats = args.N_repeats 
-
-print(args.scene_sequence)
-
 
 
 from dotenv import load_dotenv; load_dotenv()
@@ -45,35 +41,13 @@ G = G.edge_subgraph(edges_to_keep)
 largest_cc = max(nx.connected_components(G), key=len)
 G = G.subgraph(largest_cc)
 
-
-scene_dict = pd.read_csv(os.path.join(gdrive_basedir, args.song, 'prompt_data', 'scene_dict.csv'), index_col=0).to_dict()['0']
-
-# convert the values from strings to lists
-scene_dict = {k: v.split(',') for k,v in scene_dict.items()}
-
-# remove single quotes and list brackets from each element in the list
-scene_dict = {k: [re.sub(r"['\[\]]", '', fn).strip() for fn in v] for k,v in scene_dict.items()}
-
-# truncate the digits after each hyphen to 4 digits
-scene_dict = {scene: [re.sub(r'-(\d+)$', lambda m: '-' + m.group(1)[:4], fn) for fn in scene_dict[scene]] for scene in scene_dict}
-
-scene_dict
+# keep largest graph 
+largest_cc = max(nx.connected_components(G), key=len)
+G = G.subgraph(largest_cc)
 
 #%%
 
-scene_list = scene_dict.keys()
-
-# invert scene_dict to make a mapping from file to folder name
-
-file_to_scene_dict = {}
-for scene in scene_list:
-
-    for fn in scene_dict[scene]:
-        file_to_scene_dict[fn] = scene
-
-file_to_scene_dict
-
-#%%
+scene_dict, file_to_scene_dict = read_scene_dict(gdrive_basedir, args.song)
 
 for node in list(G.nodes):
     if node in file_to_scene_dict:
@@ -83,57 +57,17 @@ for node in list(G.nodes):
         print("No scene for node: {}, dropping".format(node))
         # Drop this node from the graph
         G.remove_node(node)
+        #
 
-#%%
-
-nx.draw(G)
-
-#%%
-
-
-plt.figure(figsize=(10,10))
-
-# Make a color map with a different color for each scene based on the scene of each node 
-# create number for each group to allow use of colormap
-from itertools import count
-# get unique groups
-groups = set(nx.get_node_attributes(G,'scene').values())
-mapping = dict(zip(sorted(groups),count()))
-nodes = G.nodes()
-colors = [mapping[G.nodes[n]['scene']] for n in nodes]
-
-# drawing nodes and edges separately so we can capture collection for colobar
-pos = nx.spring_layout(G)
-ec = nx.draw_networkx_edges(G, pos, alpha=0.2)
-nc = nx.draw_networkx_nodes(G, pos, nodelist=nodes, node_color=colors, node_size=100, cmap=plt.cm.jet)
-plt.colorbar(nc)
-plt.axis('off')
-# plt.show()
-plt.savefig(pjoin(gdrive_basedir, args.song, 'story', 'story_graph_2.png'))
 # %%
 fp_scene_sequence = os.path.join(gdrive_basedir, args.song, 'prompt_data', '{}.csv'.format(args.scene_sequence))
 scene_sequence = pd.read_csv(fp_scene_sequence , index_col=0)['scene'].values.tolist()
 
 # scene_sequence = scene_sequence[0:5]
 
-scene_sequence
-
 #%%
 
-# remove all edges from the graph that do not connect nodes in the scene_sequence
-
-# make a list of all edges in the graph
-all_edges = list(G.edges)
-
-# only keep edges that connect nodes with pairs of scenes that are adjacent in the scene_sequence
-adjacent_edges = [edge for edge in all_edges if G.nodes[edge[0]]['scene'] in scene_sequence and G.nodes[edge[1]]['scene'] in scene_sequence and abs(scene_sequence.index(G.nodes[edge[0]]['scene']) - scene_sequence.index(G.nodes[edge[1]]['scene'])) == 1]
-
-# also include edges that connect nodes in the same scene
-adjacent_edges.extend([edge for edge in all_edges if G.nodes[edge[0]]['scene'] == G.nodes[edge[1]]['scene']])
-
-# downselect the graph to only include these edges and their nodes
-G_sequence = G.edge_subgraph(adjacent_edges)
-
+G_sequence = downselect_to_scene_sequence(G, scene_sequence)
 
 #%%
 
@@ -184,40 +118,11 @@ for i, scene in enumerate(scene_sequence):
 
 #%%
 
-path
-
-# %%
-# draw the graph
-
-plt.figure(figsize=(10,10))
-
-# Make a color map with a different color for each scene based on the scene of each node
-# create number for each group to allow use of colormap
-
-from itertools import count
-# get unique groups
-groups = set(nx.get_node_attributes(G,'scene').values())
-mapping = dict(zip(sorted(groups),count()))
-nodes = G.nodes()
-colors = [mapping[G.nodes[n]['scene']] for n in nodes]
-
-# drawing nodes and edges separately so we can capture collection for colobar
-
-pos = nx.spring_layout(G)
-
-ec = nx.draw_networkx_edges(G, pos, alpha=0.2)
-
-nc = nx.draw_networkx_nodes(G, pos, nodelist=nodes, node_color=colors, node_size=100, cmap=plt.cm.jet)
-
-plt.colorbar(nc)
-
-# color the edges in path red 
-
 path_edges = list(zip(path,path[1:]))
 
-nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='r', width=10)
-
 #%%
+
+
 df_transitions = pd.DataFrame(path_edges, columns=['c1','c2'])
 
 # TODO: this can't be obtained from the graph?
@@ -226,74 +131,28 @@ dir_transitions = os.path.join(gdrive_basedir, args.song, 'transition_images')
 trans_list = [t for t in os.listdir(dir_transitions) if os.path.isdir(pjoin(dir_transitions,t))]
 trans_list = [image_names_from_transition(t) for t in trans_list]
 
+
+
 forward_c_pairs = trans_list
-df_transitions['reversed'] = [tuple(c_pair) not in forward_c_pairs  for c_pair in df_transitions[['c1','c2']].values]
-
-df_transitions
-
-# %%
-
-df_trans_sequence = df_transitions
-
-df_trans_sequence['input_image_folder'] = df_trans_sequence.apply(
-    lambda x: x['c1']+ ' to ' + x['c2'] if not x['reversed'] else 
-    x['c2']+ ' to ' + x['c1'],
-    axis=1
-    )
-
-# df_trans_sequence['input_movie_folder'] = ['transitions_rev' if reverse else 'transitions' for reverse in df_trans_sequence['reversed']]
 song_basedir = os.path.join(gdrive_basedir, args.song)
 
-df_trans_sequence['input_image_folder'] = song_basedir + '\\' + 'transition_images' + '\\' +  df_trans_sequence['input_image_folder']
+df_transitions = construct_input_image_folder_paths(df_transitions, song_basedir, forward_c_pairs)
 
-df_exist = df_trans_sequence['input_image_folder'].apply(os.path.exists)
-df_not_exist =df_trans_sequence.where(df_exist == False).dropna(how='all')
-
-#TODO: move file exist checks to original for loop, such that it can keep trying to make a valid superscene with partial transitions. 
-if len(df_not_exist):
-    print("Files not existing:  {}".format(df_not_exist))
-    print(df_not_exist['input_image_folder'].values)
-    raise ValueError()
-
-
-#%%
-
-out_txt = ''
-fps = 10
-image_duration = 1/fps
-
-for idx, row in df_trans_sequence.iterrows():
-
-    folder = row['input_image_folder']
-
-    images = [fn for fn in os.listdir(folder) if fn.endswith('.png')]
-    images = sorted(images)
-
-    if row['reversed']: images = images[::-1]
-
-    image_fps = [os.path.join(folder, fn) for fn in images]
-    image_fps = [fp.replace('\\', '/') for fp in image_fps]
-    image_fps = [fp.replace(' ', '\ ') for fp in image_fps]
-
-    for fp in image_fps:
-        out_txt += 'file {}\nduration {}\n'.format(fp, image_duration)
-
-
-#%%
-# out_txt = 'file ' + "\nfile ".join(image_list)
+check_input_image_folders_exist(df_transitions)
 
 out_dir = os.path.join(song_basedir, 'story')
 if not os.path.exists(out_dir): os.mkdir(out_dir)
 
-with open('videos.txt', 'w') as f:
+df_transitions.to_csv(os.path.join(out_dir, 'trans_sequence.csv'))
+
+
+#%%
+
+out_txt = generate_text_for_ffmpeg(df_transitions, fps=args.fps)
+
+# use out_text to make a text file that can be used by ffmpeg to make a movie
+
+with open(os.path.join(out_dir, 'videos.txt'), 'w') as f:
     f.write(out_txt)
 
-import shutil
-
-df_trans_sequence.to_csv(os.path.join(out_dir, 'trans_sequence.csv'))
-shutil.move('videos.txt', os.path.join(out_dir, 'videos_story.txt'))
-
-os.chdir(out_dir)
-os.system('ffmpeg -f concat -safe 0 -i videos_story.txt -c mjpeg -q:v 3 -r {} {}'.format(fps, args.output_filename))
-# %%
-
+generate_output_video(args.fps, out_dir, args.output_filename)
