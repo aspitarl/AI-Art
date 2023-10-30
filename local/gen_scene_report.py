44f81a3c96e8379b75cbbee4fd79642293d51f7e
Lee <aspitarte@gmail.com>
2023-10-30 11:58:41 -0700

    add color pallette to scene report

--- local/gen_scene_report.py
+++ local/gen_scene_report.py
@@ -56,16 +56,49 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Tabl
 
 # https://www.blog.pythonlibrary.org/2010/03/08/a-simple-step-by-step-reportlab-tutorial/
 
+from PIL import Image as PILImage
+
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 from reportlab.platypus import SimpleDocTemplate, Image, PageBreak, Table, TableStyle
+from reportlab.lib.utils import ImageReader
 
 styles = getSampleStyleSheet()
 
-def build_pdf(scene_dict, output_file):
-    doc = SimpleDocTemplate(output_file, pagesize=letter)
-    story = []
+def generate_color_scheme(filepaths):
+    # create an array to store the dominant colors from each image
+    colors = np.zeros((len(filepaths), 3))
+
+    # extract the dominant color from each image
+    for i, fp in enumerate(filepaths):
+        
+        img = PILImage.open(fp)
+
+        img = img.convert('P', palette=PILImage.ADAPTIVE, colors=4)
+        colors[i] = img.getpalette()[:3]
+
+    # create a new image with the dominant colors
+    box_width=50
+    box_height=20
+
+    img_width = len(filepaths) * box_width
+    img_height = box_height
+    color_scheme = PILImage.new('RGB', (img_width, img_height), (255, 255, 255))
+    for i, color in enumerate(colors):
+        x0 = i * box_width
+        x1 = (i + 1) * box_width
+        y0 = 0
+        y1= box_height
+        color = tuple(int(c) for c in color)
+        color_box = PILImage.new('RGB', (box_width, box_height), color)
+        color_scheme.paste(color_box, (x0, y0, x1, y1))
+
+    return color_scheme
+
+
+
+def build_scene_sequence_story(scene_dict, story=[]):
 
     for scene in scene_dict:
 
@@ -74,25 +107,34 @@ def build_pdf(scene_dict, output_file):
         # replace - with _ 
         # scene_images = [s.replace(' ', '\ ') for s in scene_images]
 
-
-        # add a page break before each new key
-        story.append(PageBreak())
-
         # add the key as a heading
-        story.append(Paragraph(scene, styles['Heading1']))
+        story.append(Paragraph("Scene: {}".format(scene), styles['Heading1']))
 
         # create a table with 2 columns and as many rows as needed for the images
         # the images have 16/9 aspect ratio, and scale them to 4 inches wide
+        filepaths = [pjoin(scene_dir, scene, image_name + '.png') for image_name in scene_images]
+        images = [Image(filepath, width=4*inch, height=4*(9/16)*inch) for filepath in filepaths]
+
+        # assume that 'color_scheme' is a Pillow image object
+        color_scheme = generate_color_scheme(filepaths)
+
+        # save the scene color theme to a file, have to save each separately
+        fp_color_scheme = pjoin(color_scheme_output_dir, 'color_scheme_{}.png'.format(scene))
+        color_scheme.save(fp_color_scheme)
+        color_scheme_flowable = Image(fp_color_scheme, width=4*inch, height=0.5*inch)
+
+        story.append(color_scheme_flowable)
+
         table_data = []
         row = []
         for i, image_name in enumerate(scene_images):
             # add each image to the table
             # img = Image(filename, width=2*inch, height=2*inch)
-            filepath = pjoin(scene_dir, scene, image_name + '.png')
+            filepath = filepaths[i]
 
             sub_table_data = [
                 [Paragraph(image_name, styles['Normal'])],
-                [Image(filepath, width=4*inch, height=4*(9/16)*inch)]
+                [images[i]]
             ]
             sub_table = Table(sub_table_data)
             row.append(sub_table)
@@ -113,13 +155,27 @@ def build_pdf(scene_dict, output_file):
 
         story.append(table)
 
-    # build the PDF
-    doc.build(story)
+            # add a page break before each new key
+        story.append(PageBreak())
+
+    return story
 
 
 output_pdf_path = os.path.join(gdrive_basedir, args.song, 'story', 'scene_report.pdf')
 
-# # only keep the first three scenes for now
-# scene_dict = {k:scene_dict[k] for k in list(scene_dict.keys())[:3]}
+#TODO: Can we make a flowable image from PIL image without saving to disk?
+color_scheme_output_dir = 'output/color_schemes'
+if not os.path.exists(color_scheme_output_dir):
+    os.makedirs(color_scheme_output_dir)
 
-story = build_pdf(scene_dict, output_pdf_path)
+# keep a random selecton of 3 scenes
+# scene_dict = {k:scene_dict[k] for k in np.random.choice(list(scene_dict.keys()), 3)}
+
+doc = SimpleDocTemplate(output_pdf_path, pagesize=letter)
+
+story = build_scene_sequence_story(scene_dict)
+
+#TODO: add to the beginning of story a table of scene_dict, with a second column of color schemes
+
+# build the PDF
+doc.build(story)
