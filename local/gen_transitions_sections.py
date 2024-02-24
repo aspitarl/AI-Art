7bf7011f84199988b18673c216c2a4a287d46ee1
aspitarl <aspitarte@gmail.com>
2024-02-24 14:52:23 -0800

    state pipey emit new transition gen path sections
    add gen_transitions_section and fix end node

--- local/gen_transitions_sections.py
+++ local/gen_transitions_sections.py
@@ -35,7 +35,7 @@ from dotenv import load_dotenv; load_dotenv(override=True)
 # %%
 
 parser = argparse.ArgumentParser()
-parser.add_argument("song", default='cycle_mask_full', nargs='?')
+parser.add_argument("song", default='emit', nargs='?')
 parser.add_argument('--ss', default='', dest='scene_sequence')
 args = parser.parse_args()
 # args = parser.parse_args("") # Needed for jupyter notebook
@@ -53,7 +53,7 @@ df_scene_sequence = pd.read_csv(fp_scene_sequence , index_col=0).reset_index(dro
 scene_sequence_list = df_scene_sequence['scene'].values.tolist()
 
 scene_dir = pjoin(gdrive_basedir, args.song, 'scenes')
-scene_dict, file_to_scene_dict = gen_scene_dicts(scene_dir, scene_sequence_list, truncate_digits=4)
+scene_dict, file_to_scene_dict = gen_scene_dicts(scene_dir, scene_sequence_list, truncate_digits=None)
 
 dir_transitions = os.path.join(gdrive_basedir, args.song, 'transition_images')
 trans_list = [t for t in os.listdir(dir_transitions) if os.path.isdir(pjoin(dir_transitions,t))]
@@ -62,32 +62,9 @@ trans_list = [image_names_from_transition(t) for t in trans_list]
 G = build_graph_scenes(scene_dict)
 G = check_existing_transitions(G, trans_list)
 
-# only keep edges with the attribute exists=True
-edges_to_keep = [(u,v) for u,v,d in G.edges(data=True) if d['exists']]
-G = G.edge_subgraph(edges_to_keep)
-
-# keep largest graph 
-largest_cc = max(nx.connected_components(G), key=len)
-G = G.subgraph(largest_cc)
-
-
 #%%
 
-for node in list(G.nodes):
-    if node in file_to_scene_dict:
-        G.nodes[node]['scene'] = file_to_scene_dict[node]
-    else:
-        # G.nodes[node]['scene'] = 'missing'
-        print("No scene for node: {}, dropping".format(node))
-        # Drop this node from the graph
-        G.remove_node(node)
-        #
-
-#%%
-
-G_sequence = downselect_to_scene_sequence(G, scene_sequence_list)
-
-
+list(G.nodes)
 
 # %%
 import re
@@ -102,7 +79,7 @@ if pd.isna(df_scene_sequence2['start'].iloc[0]):
 df_scene_sequence2['start'] = df_scene_sequence2['start'].ffill()
 
 # scene_dict = {scene: [re.sub(r'-(\d+)$', lambda m: '-' + m.group(1)[:truncate_digits], fn) for fn in scene_dict[scene]] for scene in scene_dict}
-string_truncate_length = 4
+string_truncate_length = 400
 df_scene_sequence2['start'] = [re.sub(r'_(\d+)$', lambda m: '-' + m.group(1)[:string_truncate_length], fn) for fn in df_scene_sequence2['start']]
 
 # make an ascending index based on start
@@ -121,9 +98,9 @@ df_scene_sequence2 = df_scene_sequence2.merge(temp_df[['path_section', 'end']], 
 # pick a random end for the last path_section
 
 
+#%%
 
-
-df_scene_sequence2
+list(G.nodes)
 
 #%%
 
@@ -135,6 +112,8 @@ scene_order_lookup = df_scene_sequence2['scene'].reset_index().set_index('scene'
 path = []
 path_section_list = []
 
+G_sequence = G.copy()
+
 for idx, df_path_section in df_scene_sequence2.groupby('path_section'):
 
     print("Path Section: ", idx)
@@ -145,6 +124,7 @@ for idx, df_path_section in df_scene_sequence2.groupby('path_section'):
 
         last_scene = df_path_section['scene'].iloc[-1]
         last_scene_nodes = [node for node in G_sequence.nodes if G_sequence.nodes[node]['scene'] == last_scene]
+        last_scene_nodes = [node for node in last_scene_nodes if node is not df_path_section['start'].iloc[-1]]
         end_node = np.random.choice(last_scene_nodes)
         
         subgraph = G_sequence.subgraph(path_section_nodes)
@@ -163,14 +143,14 @@ for idx, df_path_section in df_scene_sequence2.groupby('path_section'):
         subgraph = G_sequence.subgraph([*path_section_nodes, *next_scene_nodes])
 
 
-    total_duration = df_path_section['duration'].sum()
+    total_duration = df_path_section['duration'].sum() + len(df_path_section)
 
     start_node = df_path_section['start'].iloc[0]
 
     # find all simple paths between start and end, that are of total_duraiton in length
 
 
-    max_duration_add = 20
+    max_duration_add = 1
 
     for j in range(max_duration_add):
         max_duration = total_duration + j
@@ -227,16 +207,6 @@ section_list = [scene_to_section_dict[scene] for scene in scene_list]
 
 
 #%%
-
-scene_list
-
-#%%
-
-s1_nodes = [node for node in G_sequence.nodes if G_sequence.nodes[node]['scene'] == 's1']
-
-s1_nodes
-
-#%%
 path_edges = list(zip(path,path[1:]))
 path_section_list = path_section_list[1:]
 
@@ -246,25 +216,161 @@ plt.tight_layout()
 
 plt.savefig(pjoin(gdrive_basedir, args.song, 'story', 'storygraph_long.png'))
 # %%
-from aa_utils.local import gen_df_transitions, check_input_image_folders_exist
+# Check that path_edges is one connected path
+
+path_nodes = set([n for e in path_edges for n in e])
+
+G_path = nx.Graph()
+G_path.add_nodes_from(path_nodes)
+G_path.add_edges_from(path_edges)
+
+nx.is_connected(G_path)
+
+nx.draw(G_path)
+
+#%%
+# Having to check existing separately so that output transitions file are not truncated to 4 digits
+# TODO: rework seed length to avoid this and truncation in geenral
+# TODO: this can't be obtained from the graph?
+dir_transitions = os.path.join(gdrive_basedir, args.song, 'transition_images')
+trans_list = [t for t in os.listdir(dir_transitions) if os.path.isdir(pjoin(dir_transitions,t))]
+trans_list = [image_names_from_transition(t) for t in trans_list]
+
+# make a mapping from node name to truncated name for each node in the graph
+
+node_to_trunc = {n: n.split('-')[0] + '-' + n.split('-')[1][:4] for n in G.nodes}
+
+G_plot = G.copy()
+G_plot = nx.relabel_nodes(G_plot, node_to_trunc)
+G_plot = check_existing_transitions(G_plot, trans_list)
+path_edges_truncate = [(node_to_trunc[e[0]], node_to_trunc[e[1]]) for e in path_edges]
+
+plot_scene_sequence(G_plot, scene_sequence_list, scene_dict, path_edges=path_edges_truncate)
+
+plt.savefig(pjoin(gdrive_basedir, args.song, 'story', 'story_transition_gen.png'))
+
+# %%
+from aa_utils.local import gen_df_transitions
 
 song_basedir = os.path.join(gdrive_basedir, args.song)
 out_dir = os.path.join(song_basedir, 'story')
 if not os.path.exists(out_dir): os.makedirs(out_dir)
 
-df_transitions = gen_df_transitions(G_sequence,path_edges,section_list,song_basedir)
+scene_section_map = df_scene_sequence.set_index('scene')['section']
+section_list = [scene_section_map[G.nodes[e[0]]['scene']] for e in path_edges]
+section_list.append(scene_section_map[G.nodes[path_edges[-1][1]]['scene']])
 
-# add path section list as second column
-df_transitions['path_section'] = path_section_list
-first_cols = ['path_section','section']
-cols = first_cols + [col for col in df_transitions if col not in first_cols]
-df_transitions = df_transitions[cols]
+df_transitions = gen_df_transitions(G,path_edges,section_list,song_basedir)
 
-check_input_image_folders_exist(df_transitions)
+df_transitions.to_csv(os.path.join(out_dir, 'trans_sequence_gen.csv'))
 
-df_transitions.to_csv(os.path.join(out_dir, 'trans_sequence.csv'))
+#%%
+# iterate through path_edges and split into inter and intra scene edges
 
+
+interscene_edges = []
+intrascene_edges = []
+
+for edge in path_edges:
+    scene_from = G.nodes[edge[0]]['scene']
+    scene_to = G.nodes[edge[1]]['scene']
+    if scene_from == scene_to:
+        intrascene_edges.append(edge)
+    else:
+        interscene_edges.append(edge)
+
+
+intrascene_edges = list(set(intrascene_edges))
+interscene_edges = list(set(interscene_edges))
 
 # %%
-df_transitions
+
+# Interscene transition file
+
+# split each edge into two list of nodes_from and nodes_to
+
+nodes_from = [e[0] for e in interscene_edges]
+nodes_to = [e[1] for e in interscene_edges]
+scenes_from = [G.nodes[n]['scene'] for n in nodes_from]
+scenes_to = [G.nodes[n]['scene'] for n in nodes_to]
+
+# make a dataframe wit nodes_from and nodes_to as columns
+
+df_inter = pd.DataFrame({'nodes_from':nodes_from, 'nodes_to':nodes_to})
+
+# Split the nodes_from and nodes_to columns with the hyphen in each node name 
+
+df_inter[['from_name', 'from_seed']] = df_inter['nodes_from'].str.split('-', expand=True)
+df_inter[['to_name', 'to_seed']] = df_inter['nodes_to'].str.split('-', expand=True)
+
+# drop the nodes_from an nodes_to columns
+
+df_inter = df_inter.drop(columns=['nodes_from', 'nodes_to'])
+
+df_inter['compute'] = 'y'
+df_inter['duration'] = 5
+df_inter['scene_from'] = scenes_from
+df_inter['scene_to'] = scenes_to
+
+
+df_inter['from_seed'] = df_inter['from_seed'].astype(str)
+df_inter['to_seed'] = df_inter['to_seed'].astype(str)
+
+df_inter = df_inter.sort_values('scene_from', key=lambda x: x.map(scene_sequence_list.index))
+df_inter = df_inter.reset_index(drop=True)
+
+
+fp_out = os.path.join(gdrive_basedir, args.song, 'prompt_data', 'interscene_transitions.csv')
+print("writing transitions csv to {}".format(fp_out))
+df_inter.to_csv(fp_out)
+
 # %%
+# Intrascene transition file
+
+if len(intrascene_edges) == 0:
+    df_intra = pd.DataFrame(columns = ['from_name','from_seed','to_name','to_seed', 'compute', 'duration', 'scene'])
+
+else:
+
+    # split each edge into two list of nodes_from and nodes_to
+
+    nodes_from = [e[0] for e in intrascene_edges]
+    nodes_to = [e[1] for e in intrascene_edges]
+    scenes_from = [G.nodes[n]['scene'] for n in nodes_from]
+    scenes_to = [G.nodes[n]['scene'] for n in nodes_to]
+
+    # assert that scenes_from and scenes_to are the same
+
+    assert all([scenes_from[i] == scenes_to[i] for i in range(len(scenes_from))])
+
+    # make a dataframe wit nodes_from and nodes_to as columns
+
+    df_intra = pd.DataFrame({'nodes_from':nodes_from, 'nodes_to':nodes_to})
+
+    # Split the nodes_from and nodes_to columns with the hyphen in each node name
+
+    df_intra[['from_name', 'from_seed']] = df_intra['nodes_from'].str.split('-', expand=True)
+
+    df_intra[['to_name', 'to_seed']] = df_intra['nodes_to'].str.split('-', expand=True)
+
+    # drop the nodes_from an nodes_to columns
+
+    df_intra = df_intra.drop(columns=['nodes_from', 'nodes_to'])
+
+    df_intra['compute'] = 'y'
+    df_intra['duration'] = 5
+    df_intra['scene'] = scenes_from
+
+    df_intra['from_seed'] = df_intra['from_seed'].astype(str)
+    df_intra['to_seed'] = df_intra['to_seed'].astype(str)
+
+    df_intra = df_intra.sort_values('scene', key=lambda x: x.map(scene_sequence_list.index))
+    df_intra = df_intra.reset_index(drop=True)
+
+    df_intra
+
+
+fp_out = os.path.join(gdrive_basedir, args.song, 'prompt_data', 'intrascene_transitions.csv')
+print("writing transitions csv to {}".format(fp_out))
+df_intra.to_csv(fp_out)
+
