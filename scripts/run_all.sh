1b0fb365c5c4c3de1d571948296ecf5b8e1d2cda
Lee <aspitarte@gmail.com>
2025-01-25 22:39:11 +0000

    various minor getting lambda running

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,8 +1,8 @@
 song_name=$1
 
 scripts=(
-    "image_gen/gen_saved_prompts.py" 
-    "story_gen/automake_scenes.py" 
+    # "image_gen/gen_saved_prompts.py" 
+    # "story_gen/automake_scenes.py" 
     "story_gen/gen_transition_meta.py" 
     "story_gen/examine_existing_transitions.py" 
     "image_gen/gen_transitions.py"
