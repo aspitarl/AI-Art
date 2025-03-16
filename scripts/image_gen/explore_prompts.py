c42adac6b60393098221802c01b32d3af3065573
Lee Aspitarte <aspitarte@gmail.com>
2025-03-16 21:15:55 +0000

    simplify explore prompts

--- scripts/image_gen/explore_prompts.py
+++ scripts/image_gen/explore_prompts.py
@@ -19,19 +19,14 @@ dotenv.load_dotenv()
 
 parser = argparse.ArgumentParser(description='Generate transitions between prompts')
 parser.add_argument('song_name', type=str, help='The name of the song to generate transitions for')
+parser.add_argument('--prompt_name', '-p', type=str, required=True)
 parser.add_argument('--setting_name', '-sn', type=str, default='default', nargs='?', help='Name of top-level key in settings json')
-parser.add_argument('--prompt_name', '-p', type=str, default="geo1")
 parser.add_argument('--num_images', '-n', type=int, default=4)
 args = parser.parse_args()
 # args=None
 
-song_name = args.song_name if args.song_name else 'escape'
-num_images = args.num_images if args.num_images else 4
-
-
-
 #%%
-song_meta_dir = os.path.join(os.getenv('meta_dir'), song_name)
+song_meta_dir = os.path.join(os.getenv('meta_dir'), args.song_name)
 
 settings = load_settings_json(song_meta_dir, setting_name=args.setting_name)
 
@@ -39,12 +34,9 @@ df_prompt = load_df_prompt(song_meta_dir, seed_delimiter=settings['seed_delimite
 
 pipe = gen_pipe(settings)
 
-default_prompt = 'geo1'
-name_sel = args.prompt_name if args.prompt_name else default_prompt
+prompt = df_prompt['prompt'][args.prompt_name]
 
-prompt = df_prompt['prompt'][name_sel]
-
-pipe_kwargs = gen_pipe_kwargs_static(df_prompt.loc[name_sel], settings['pipe_name'], song_name)
+pipe_kwargs = gen_pipe_kwargs_static(df_prompt.loc[args.prompt_name], settings['pipe_name'], args.song_name)
 settings['pipe_kwargs'].update(pipe_kwargs)
 
 col_wrap = 2 
@@ -64,7 +56,7 @@ rows, cols = calculate_rows_cols(args.num_images, col_wrap)
 # Make new random seeds in a hacky way. TODO: probably a function to generate seeds without making a generator instance.
 
 generator = torch.Generator(device="cuda")
-seeds = [generator.seed() for i in range(num_images)]
+seeds = [generator.seed() for i in range(args.num_images)]
 # truncate seeds to 4 digits
 seeds = [int(str(seed)[:4]) for seed in seeds]
 
@@ -84,11 +76,7 @@ images = pipe(
 
 grid = image_grid(images, rows=rows, cols=cols)
 
-grid
-# %%
-
-
-output_dir = os.path.join(os.getenv('media_dir'), song_name, 'explore_images', name_sel)
+output_dir = os.path.join(os.getenv('media_dir'), args.song_name, 'explore_images', args.prompt_name)
 if not os.path.exists(output_dir): os.makedirs(output_dir)
 
 grid.save(os.path.join(output_dir, 'image_grid.png'))
